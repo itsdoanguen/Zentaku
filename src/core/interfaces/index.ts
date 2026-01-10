053b0b06bcc778c8613cd8c0c069c588f3c241a2
itsdoanguen <doannguyennguyen654@gmail.com>
2026-01-10 19:27:39 +0700

    feat: add interface barrel exports
    - Export all core interfaces from single entry point
    - Simplify import statements across application
    - Improve maintainability and discoverability

---
+++ src/core/interfaces/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Core Interfaces Index
+ *
+ * Central export point for all interfaces
+ */
+
+export * from './IController';
+export * from './IMangaSource';
+export * from './IMetadataSource';
+export * from './IRepository';
+export * from './IService';
+export * from './IStreamingSource';
